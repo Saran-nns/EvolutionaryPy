71f53e286d1dff0d4c632224ee889cc275d57c04
Saran N Subramaniyan <sarannsworld@gmail.com>
2017-06-06 05:11:28 +0530

    Delete test.py

--- Image_evolve/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import random
-import os
-from scipy.ndimage import imread
-img=os.path.join('C:/Users/Saran/!Imgevolve/image.png')
-in_image=np.uint8(imread(img,flatten=False,mode='RGB'))
-#in_image=np.uint8(in_image)
-print (np.shape(in_image))
-
